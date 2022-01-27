2a2b6ecbba287298d83c26bbb59179c684163efc
Kody Deda <kodydeda4@gmail.com>
2022-01-26 19:44:34 -0500

    cleanup

---
+++ components/education-card.jsx
@@ -0,0 +1,80 @@
+import {
+  Box,
+  HStack,
+  Image as ChakraImage,
+  Link,
+  Text,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+
+const EducationCard = ({ props: { degree, institution, timeline, icon, link }}) => {
+  return (
+    <Link width="full" href={link} unstyled>
+      <HStack
+        p={4}
+        bg={useColorModeValue("gray.100", "whiteAlpha.100")}
+        rounded="lg"
+        borderWidth="1px"
+        borderColor={useColorModeValue("neutral.400", "neutralD.400")}
+        w="100%"
+        textAlign="left"
+        align="start"
+        spacing={4}
+      >
+        <IconView icon={icon} />
+        <DetailView
+          title={degree}
+          description={institution}
+          timeline={timeline}
+        />
+      </HStack>
+    </Link>
+  );
+};
+
+const IconView = ({ icon }) => {
+  return (
+    <Box
+      rounded="lg"
+      p={2}
+      position="relative"
+      overflow="hidden"
+      lineHeight={0}
+      rounded="lg"
+      boxShadow="inset 0 0 1px 1px rgba(0, 0, 0, 0.015)"
+    >
+      <Box
+        bg={useColorModeValue("gray.300", "whiteAlpha.300")}
+        position="absolute"
+        top={0}
+        bottom={0}
+        left={0}
+        right={0}
+        opacity={0.25}
+      ></Box>
+      <ChakraImage
+        src={icon}
+        height={12}
+        width={12}
+        layout="fixed"
+        rounded="md"
+      ></ChakraImage>
+    </Box>
+  );
+};
+
+const DetailView = ({ title, description, timeline }) => {
+  return (
+    <VStack align="start" justify="flex-start" spacing={1} maxW="lg" h="100%">
+      <VStack spacing={0} align="start" flexGrow="1">
+        <Text fontWeight="bold" fontSize="md" noOfLines={2}>{title}</Text>
+        <Text fontSize="sm">{description}</Text>
+        <Text fontSize="xs" color={useColorModeValue("gray.700", "whiteAlpha.700")}>{timeline}</Text>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default EducationCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,131 @@ import {
   HStack,
   Tag,
   Text,
+  useBreakpointValue,
   useColorMode,
   VStack,
 } from "@chakra-ui/react";
 import ExperienceCard from "../components/experience-card";
+import ProjectCard from "../components/project-card";
+import EducationCard from "../components/education-card";
+
+const resume = {
+  name: "Kody Deda",
+  title: "iOS Developer",
+  description:
+    "👋 Hi, I'm a professional software engineer who specializes in developing software for the Apple ecosystem. I love UI/UX design & functional programming!",
+  skills: [
+    "SwiftUI",
+    "Combine",
+    // "Composable Architecture",
+    // "MVVM-CleanArchitecture",    
+    "Firebase",
+    "Stripe",
+    // "Typescript",
+    "ReactJS",
+    // "Functional Programming",
+  ],
+  experience: [
+    {
+      title: "iOS Developer",
+      description: "Dealerware - Austin TX",
+      timeline: "July 12, 2021 - Present",
+      icon: "http://incitrio.com/wp-content/uploads/2015/01/Apple_gray_logo.png",
+      link: "https://www.dealerware.com",
+    },
+  ],
+  projects: [
+    {
+      title: "SwiftUI-FireTodos",
+      description:
+        "SwiftUI app that connects to Firebase authentication and Cloud Firestore. Features Apple, Email, and Anonymous sign-in.",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556054.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "React-FireTodos",
+      description: "React-based counterpart with Stripe integration",
+      tags: ["ReactJS"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556061.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "Sapphire",
+      description:
+        "SwiftUI app that allows you to customize your app-icons and make them look more like iOS.",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "Emerald",
+      description:
+        "SwiftUI app that allows you to set keyboard shortcuts for things like focusing, resizing, and rebalancing your windows.",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "GuitarTuner",
+      description: "Description",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "MusicToy",
+      description: "Description",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "Cookable",
+      description: "Description",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "MyersBriggs",
+      description: "Description",
+      tags: ["SwiftUI"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "DemonQuest",
+      description: "Description",
+      tags: ["Unity", "C#"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+    {
+      title: "OpenCV",
+      description: "Description",
+      tags: ["OpenCV", "Python"],
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556077.png",
+      link: "https://www.dealerware.com",
+    },
+  ],
+  education: [
+    {
+      degree: "Bachelor's of Computer Science",
+      institution: "University of North Carolina Wilmnington",
+      timeline: "Spring 2019 - Fall 2021",
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556054.png",
+    },
+    {
+      degree: "Associate's of Science",
+      institution: "Cape Fear Community College",
+      timeline: "Spring 2016 - Fall 2018",
+      icon: "https://cdn-icons-png.flaticon.com/512/6556/6556248.png",
+    },
+  ],
+};
 
 export default function IndexView() {
-  const resume = {
-    name: "Kody Deda",
-    title: "iOS Developer",
-    description:
-      "👋 Hi, I'm a professional software engineer who specializes in developing software for the Apple ecosystem. I love UI/UX design & functional programming!",
-    skills: ["SwiftUI", "Combine", "React"],
-  };
-
   return (
     <Container maxWidth="container.xl" padding={0}>
       <Flex width={"50vh"} direction={{ base: "column", md: "row" }}>
@@ -43,7 +154,9 @@ export default function IndexView() {
               skills: resume.skills,
             }}
           />
-          <ExperienceView />
+          <ExperienceView props={{ experience: resume.experience }} />
+          <ProjectsView props={{ projects: resume.projects }} />
+          <EducationView props={{ education: resume.education }} />
         </VStack>
       </Flex>
     </Container>
@@ -79,31 +192,61 @@ const AboutView = ({ props: { description, skills } }) => {
   );
 };
 
-const ExperienceView = () => {
-  const props = {
-    experiences: [
-      {
-        title: "iOS Developer",
-        description: "Dealerware - Austin TX",
-        timeline: "July 12, 2021 - Present",
-        icon: "http://incitrio.com/wp-content/uploads/2015/01/Apple_gray_logo.png",
-        link: "https://www.dealerware.com",
-      },
-    ],
-  };
-
+const ExperienceView = ({ props: { experience } }) => {
   return (
     <VStack width="full" spacing={3} alignItems={"flex-start"}>
       <Heading size="lg">Experience</Heading>
       <Divider />
-      {props.experiences.map((experience) => (
+      {experience.map((i) => (
         <ExperienceCard
           props={{
-            title: experience.title,
-            description: experience.description,
-            timeline: experience.timeline,
-            icon: experience.icon,
-            link: experience.link,
+            title: i.title,
+            description: i.description,
+            timeline: i.timeline,
+            icon: i.icon,
+            link: i.link,
+          }}
+        />
+      ))}
+    </VStack>
+  );
+};
+
+const ProjectsView = ({ props: { projects } }) => {
+  const colSpan = useBreakpointValue({ base: 2, md: 1 });
+
+  return (
+    <VStack width="full" spacing={3} alignItems={"flex-start"}>
+      <Heading size="lg">Projects</Heading>
+      <Divider />
+      {projects.map((i) => (
+        <ProjectCard
+          props={{
+            title: i.title,
+            description: i.description,
+            tags: i.tags,
+            icon: i.icon,
+            link: i.link,
+          }}
+        />
+      ))}
+    </VStack>
+  );
+};
+
+const EducationView = ({ props: { education } }) => {
+  return (
+    <VStack width="full" spacing={3} alignItems={"flex-start"}>
+      <Heading size="lg">Education</Heading>
+      <Divider />
+      {education.map((i) => (
+        <EducationCard
+          props={{
+            degree: i.degree,
+            institution: i.institution,
+            timeline: i.timeline,
+            icon: i.icon,
+            link: i.link,
           }}
         />
       ))}
