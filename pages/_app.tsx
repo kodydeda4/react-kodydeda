b7112d07863d8db7bf353cb1d37b5025cef3bc03
Kody Deda <kodydeda4@gmail.com>
2022-02-01 16:26:09 -0500

    cleanup App.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
-import { ChakraProvider, Link } from '@chakra-ui/react'
-import { Head } from 'next/document'
-import useAppTheme from '../hooks/useAppTheme'
+import { ChakraProvider } from "@chakra-ui/react";
+import useAppTheme from "../hooks/useAppTheme";
 
 function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={useAppTheme()}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
-export default App
+export default App;
